f62c7f659e0b3c03e2f7ca16de54cf0df59cd48e
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-16 14:50:38 -0500

    Implemented advanced filter options but still need to create a submit class to capture results. (VPP)

--- src/Components/AdvancedFilter/AdvancedFilter.jsx
+++ src/Components/AdvancedFilter/AdvancedFilter.jsx
@@ -9,9 +9,31 @@ class AdvancedFilter extends Component {
 
     render() {
         return(
-            <div class="form-floating mb-3">
-                <input type="songtitle" class="form-control" id="floatingInput" placeholder="Song Title"/>
-                <label for="floatingInput">Song Title</label>
+            <div className="AdvancedFilterInput">
+                <div className="form-floating mb-3">
+                    <input type="songTitle" className="form-control" id="floatingInput" placeholder="Song Title"/>
+                    <label htmlFor="floatingInput">Song Title</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input type="album" className="form-control" id="floatingInput" placeholder="Album"/>
+                    <label htmlFor="floatingInput">Album</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input type="artist" className="form-control" id="floatingInput" placeholder="Artist"/>
+                    <label htmlFor="floatingInput">Artist</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input type="genre" className="form-control" id="floatingInput" placeholder="Genre"/>
+                    <label htmlFor="floatingInput">Genre</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input type="releaseDate" className="form-control" id="floatingInput" placeholder="Release Date"/>
+                    <label htmlFor="floatingInput">Release Date</label>
+                </div>
             </div>
         )
     }
