31b9f133411f379144d9b17d7815fb1e2c2e5956
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-16 15:15:45 -0500

    Finalized filter bar.(VPP)

--- src/Components/AdvancedFilter/AdvancedFilter.jsx
+++ src/Components/AdvancedFilter/AdvancedFilter.jsx
@@ -9,30 +9,35 @@ class AdvancedFilter extends Component {
 
     render() {
         return(
-            <div className="AdvancedFilterInput">
+            <div className="AdvancedFilterPosition">
                 <div className="form-floating mb-3">
-                    <input type="songTitle" className="form-control" id="floatingInput" placeholder="Song Title"/>
                     <label htmlFor="floatingInput">Song Title</label>
+                    <input type="songTitle" className="form-control" id="floatingInput" placeholder="Song Title"/> 
                 </div>
 
                 <div className="form-floating mb-3">
-                    <input type="album" className="form-control" id="floatingInput" placeholder="Album"/>
                     <label htmlFor="floatingInput">Album</label>
+                    <input type="album" className="form-control" id="floatingInput" placeholder="Album"/>
                 </div>
 
                 <div className="form-floating mb-3">
-                    <input type="artist" className="form-control" id="floatingInput" placeholder="Artist"/>
                     <label htmlFor="floatingInput">Artist</label>
+                    <input type="artist" className="form-control" id="floatingInput" placeholder="Artist"/>                    
                 </div>
 
                 <div className="form-floating mb-3">
-                    <input type="genre" className="form-control" id="floatingInput" placeholder="Genre"/>
                     <label htmlFor="floatingInput">Genre</label>
+                    <input type="genre" className="form-control" id="floatingInput" placeholder="Genre"/>
+                    
                 </div>
 
                 <div className="form-floating mb-3">
-                    <input type="releaseDate" className="form-control" id="floatingInput" placeholder="Release Date"/>
                     <label htmlFor="floatingInput">Release Date</label>
+                    <input type="releaseDate" className="form-control" id="floatingInput" placeholder="Release Date"/>                    
+                </div>
+
+                <div className="SearchButtonPosition">
+                    <input class="btn btn-primary" type="button" value="Search"/>
                 </div>
             </div>
         )
